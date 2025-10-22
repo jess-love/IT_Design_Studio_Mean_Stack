38444ea16757a59d47aaeeb9ecb49a05ba3646c9
Linda Emekwue <lemekwu1@my.westga.edu>
2025-10-21 22:07:15 -0400

    Resolve merge conflicts in app.ts

--- mean-project/src/app/app.ts
+++ mean-project/src/app/app.ts
@@ -1,19 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-=======
 import { ReminderComponent } from './reminders/reminder.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, ReminderComponent],
->>>>>>> Linda's-code
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('mean-project');
