23ae94e22b01f99fe2021b1810322fff7a4186d8
Manny Blain <mannyblain95@gmail.com>
2025-10-22 17:06:43 -0400

    Assignment-deadline tracker

--- mean-project/src/app/app.ts
+++ mean-project/src/app/app.ts
@@ -1,11 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AssignmentTracker } from './components/assignment-tracker/assignment-tracker'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, 
+            AssignmentTracker,
+            ReactiveFormsModule
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('mean-project');
