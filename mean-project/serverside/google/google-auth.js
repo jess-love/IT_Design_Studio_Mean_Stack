d5e5a90444c15f614f89bfa5a6a1703637ab710b
Linda Emekwue <lemekwu1@my.westga.edu>
2025-12-03 00:04:09 -0500

    saving my changes before pulling main

--- mean-project/serverside/google/google-auth.js
+++ mean-project/serverside/google/google-auth.js
@@ -3,7 +3,7 @@ const { google } = require('googleapis');
 const oauth2Client = new google.auth.OAuth2(
   "1081266735348-31omffhqt27rsosejv0d50fgt6mmq0or.apps.googleusercontent.com",
   "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",
-  "http://localhost:8000/auth/google/callback"
+  "http://localhost:3000/auth/google/callback"
 );
 
 module.exports = oauth2Client;
